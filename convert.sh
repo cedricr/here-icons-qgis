3493653aa4122eb1ac7e2e980c2440f5e853412e
Cedric Rossi <cedric@terramater.net>
2021-11-07 16:33:02 +0100

    Copy preview image to its expected location

--- convert.sh
+++ convert.sh
@@ -3,7 +3,7 @@
 for COLLECTION in $(ls icons)
 do
   TARGET_DIR=collections/here-icons/$COLLECTION
-  mkdir $TARGET_DIR
+  mkdir -p $TARGET_DIR
   cp -a icons/$COLLECTION/SVG/*.svg $TARGET_DIR 
 done
 
@@ -20,3 +20,7 @@ cp -a icons/guidance-icons/manoeuvers/*.svg collections/here-icons/manoeuvers/
 cp -a icons/guidance-icons/manoeuvers/wego-fallback-roundabout/*.svg collections/here-icons/manoeuvers/
 mkdir collections/here-icons/speed-limit/
 cp -a icons/guidance-icons/speed-limit/*.svg collections/here-icons/speed-limit/
+
+# Preview image
+mkdir -p collections/here-icons/preview
+cp -a images/overview.svg collections/here-icons/preview
