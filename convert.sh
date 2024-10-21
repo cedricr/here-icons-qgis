c7bf7d405446e27291a12094b9b292c5a5274288
Cedric Rossi <cedric@i9n.fr>
2024-10-21 17:58:10 +0200

    Icons moved to the expected `svg` directory

--- convert.sh
+++ convert.sh
@@ -7,24 +7,24 @@ SRC=here-icons
 for COLLECTION in $(ls $SRC/icons)
 do
   if [ -d "$SRC/icons/$COLLECTION/SVG" ]; then
-    TARGET_DIR=collections/here-icons/$COLLECTION
+    TARGET_DIR=collections/here-icons/svg/$COLLECTION
     mkdir -p $TARGET_DIR
     cp -a $SRC/icons/$COLLECTION/SVG/*.svg $TARGET_DIR 
   fi
 done
 
 # These folders don't follow the general structure, do them by hand
-mkdir collections/here-icons/guidance-lane-directions/
-cp -a $SRC/icons/guidance-icons/lane-assistance/directions/set-1/*.svg collections/here-icons/guidance-lane-directions/
-cp -a $SRC/icons/guidance-icons/lane-assistance/directions/set-2/*.svg collections/here-icons/guidance-lane-directions/
-mkdir collections/here-icons/guidance-lane-dividers/
-cp -a $SRC/icons/guidance-icons/lane-assistance/dividers/*.svg collections/here-icons/guidance-lane-dividers/
-mkdir collections/here-icons/guidance-lane-lanes/
-cp -a $SRC/icons/guidance-icons/lane-assistance/lanes/*.svg collections/here-icons/guidance-lane-lanes/
-mkdir collections/here-icons/manoeuvers/
-cp -a $SRC/icons/guidance-icons/manoeuvers/SVG/*.svg collections/here-icons/manoeuvers/
-mkdir collections/here-icons/road-signs/
-cp -a $SRC/icons/guidance-icons/road-signs/*.svg collections/here-icons/road-signs/
+mkdir collections/here-icons/svg/guidance-lane-directions/
+cp -a $SRC/icons/guidance-icons/lane-assistance/directions/set-1/*.svg collections/here-icons/svg/guidance-lane-directions/
+cp -a $SRC/icons/guidance-icons/lane-assistance/directions/set-2/*.svg collections/here-icons/svg/guidance-lane-directions/
+mkdir collections/here-icons/svg/guidance-lane-dividers/
+cp -a $SRC/icons/guidance-icons/lane-assistance/dividers/*.svg collections/here-icons/svg/guidance-lane-dividers/
+mkdir collections/here-icons/svg/guidance-lane-lanes/
+cp -a $SRC/icons/guidance-icons/lane-assistance/lanes/*.svg collections/here-icons/svg/guidance-lane-lanes/
+mkdir collections/here-icons/svg/manoeuvers/
+cp -a $SRC/icons/guidance-icons/manoeuvers/SVG/*.svg collections/here-icons/svg/manoeuvers/
+mkdir collections/here-icons/svg/road-signs/
+cp -a $SRC/icons/guidance-icons/road-signs/*.svg collections/here-icons/svg/road-signs/
 
 # Preview image
 mkdir -p collections/here-icons/preview
