757612d05cc15a605ecdaa1487cb987277d4a23e
Cedric Rossi <cedric@i9n.fr>
2024-10-21 17:34:37 +0200

    Update icons from upstream

--- convert.sh
+++ convert.sh
@@ -6,9 +6,11 @@ git submodule update
 SRC=here-icons
 for COLLECTION in $(ls $SRC/icons)
 do
-  TARGET_DIR=collections/here-icons/$COLLECTION
-  mkdir -p $TARGET_DIR
-  cp -a $SRC/icons/$COLLECTION/SVG/*.svg $TARGET_DIR 
+  if [ -d "$SRC/icons/$COLLECTION/SVG" ]; then
+    TARGET_DIR=collections/here-icons/$COLLECTION
+    mkdir -p $TARGET_DIR
+    cp -a $SRC/icons/$COLLECTION/SVG/*.svg $TARGET_DIR 
+  fi
 done
 
 # These folders don't follow the general structure, do them by hand
@@ -20,10 +22,9 @@ cp -a $SRC/icons/guidance-icons/lane-assistance/dividers/*.svg collections/here-
 mkdir collections/here-icons/guidance-lane-lanes/
 cp -a $SRC/icons/guidance-icons/lane-assistance/lanes/*.svg collections/here-icons/guidance-lane-lanes/
 mkdir collections/here-icons/manoeuvers/
-cp -a $SRC/icons/guidance-icons/manoeuvers/*.svg collections/here-icons/manoeuvers/
-cp -a $SRC/icons/guidance-icons/manoeuvers/wego-fallback-roundabout/*.svg collections/here-icons/manoeuvers/
-mkdir collections/here-icons/speed-limit/
-cp -a $SRC/icons/guidance-icons/speed-limit/*.svg collections/here-icons/speed-limit/
+cp -a $SRC/icons/guidance-icons/manoeuvers/SVG/*.svg collections/here-icons/manoeuvers/
+mkdir collections/here-icons/road-signs/
+cp -a $SRC/icons/guidance-icons/road-signs/*.svg collections/here-icons/road-signs/
 
 # Preview image
 mkdir -p collections/here-icons/preview
